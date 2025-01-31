587c6c03758f4b3b6a48b65f63157cb6a53dfa60
Umesh Deogharia <143274287+Umesh-Deogharia@users.noreply.github.com>
2025-01-31 22:56:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+
+let arr = [1,2,3,4];
+
+let promise = new promise(resolve , reject)=>{
+	setTImeout(()=>{
+	let oddNumbers = arr.filter((age)=>{
+		return age % 2 !== 0;
+	});
+	},3000);
+
+	setTimeout(()=>{
+		
+	})
+		
+	
+}
