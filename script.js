f76d80a5a0836f808319b187a15af97f9f9265dc
Umesh Deogharia <143274287+Umesh-Deogharia@users.noreply.github.com>
2025-02-21 23:48:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,20 +2,49 @@
 
 let arr = [1,2,3,4];
 
-let promise = new Promise(resolve , reject)=>{
-	setTImeout(()=>{
-	let evenNumbers = arr.filter((age)=> age % 2 === 0);
+let promise = new Promise((resolve,reject)=> {
+	setTimeout(()=>{
+		resolve(arr);
+	},3000)
+}
+)
+.then ((even)=>{
+	return new Promise((resolve)=> {
+		
+	setTimeout(()=>{
+	let evenNumbers = even.filter((age)=> age % 2 === 0);
 		document.querySelector("#output").innerText = evenNumbers;
 		resolve(evenNumbers);
 	},1000);
-}.then((res)=>{
-
-	return new Promise(resolve,reject)=>{
-		setTimeout(()=>{
-			let maultiplied = res.map(num => num*2);
-			document.querySelector("#output").innerText = maultiplied;
-		resolve(maultiplied);
-},3000)
-	}
-
+	})
 })
+.then((multiply)=>{
+	return new Promise((resolve)=> {
+
+	setTimeout(()=>{
+			let multiplied = multiply.map(num => num*2);
+			document.querySelector("#output").innerText = multiplied;
+		resolve(multiplied);
+},2000)
+		
+})
+})
+.catch(rej=>console.log("error",rej));
+
+// let promise = new Promise(resolve , reject)=>{
+// 	setTImeout(()=>{
+// 	let evenNumbers = arr.filter((age)=> age % 2 === 0);
+// 		document.querySelector("#output").innerText = evenNumbers;
+// 		resolve(evenNumbers);
+// 	},1000);
+// }.then((res)=>{
+
+// 	return new Promise(resolve,reject)=>{
+// 		setTimeout(()=>{
+// 			let maultiplied = res.map(num => num*2);
+// 			document.querySelector("#output").innerText = maultiplied;
+// 		resolve(maultiplied);
+// },3000)
+// 	}
+
+// })
